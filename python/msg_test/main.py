11658ebbeeabdd0594328342b4c5254adbb4d5e8
sedihglow <jamesmross4@gmail.com>
2021-10-10 12:43:40 -0700

    moved around files from the QEMU testing functionality.

---
+++ python/msg_test/main.py
@@ -0,0 +1,27 @@
+from arduino_serial import arduino_com
+from command import command_msg
+from command import command_interface
+
+import time
+
+BAUD_RATE = 115200
+SERIAL_PORT = "/dev/ttyACM0"
+RTIMEOUT = 0.5 # read serial timeout
+
+if __name__ == "__main__":
+    command = command_interface()
+    cmd_msg = command_msg()
+    arduino_serial = arduino_com(SERIAL_PORT, BAUD_RATE, RTIMEOUT)
+    arduino_serial.begin()
+
+    msg = cmd_msg.build_cmd_msg(cmd_msg.S0_ANGLE, 90) 
+
+    arduino_serial.write(msg)
+    print("reading message from arduino")
+    time.sleep(1)
+    data = arduino_serial.read_line()
+    print(data)
+    
+    # data should read empty in this iteration based on arduino code
+    data = arduino_serial.read_line()
+    print(data)
