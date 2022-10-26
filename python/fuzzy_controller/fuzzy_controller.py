572a92b17d229be6c74034be92dd62d8e367ee06
sedihglow <jamesmross4@gmail.com>
2022-10-26 08:47:38 -0700

    tested setting up the fuzzy set with different lengths as well. works as intended. may add more information per set element if needed at some point so the two elements in hand for example arent identical other than name. Maybe something representing what the value is at its start, end and mid points just so its clear on the start and end of some sets

--- python/fuzzy_controller/fuzzy_controller.py
+++ python/fuzzy_controller/fuzzy_controller.py
@@ -6,7 +6,14 @@ class fuzzy_set:
 	FUZZY_SET = []
 
 	def __init__(self, set_name, set_xstart, set_xend, set_element_names):
+		# odd even result of (val % 2)
+		ODD = 1
+		EVEN = 0
+
 		self.set_name = set_name
+
+		num_elements = len(set_element_names)
+		half_num_elements = num_elements / 2
 		
 		# next_half_point example, if set_xend = 12, set_xstart = 6, and
 		# num_elements = 7, then
@@ -19,7 +26,6 @@ class fuzzy_set:
 		# next point is 4.
 		next_point = next_half_point * 2
 
-		num_elements = len(set_element_names)
 		
 		# get first half of membership function points, every other starting
 		# at index 0
@@ -27,38 +33,59 @@ class fuzzy_set:
 		#			   {FL, CL, CR, FR}
 		k = set_xstart
 		name = 0
-		for i in range(0, math.ceil(num_elements / 2)):
-			xstart = k
-			xend   = k + next_point
+		for i in range(0, math.ceil(half_num_elements)):
 			fuzzy_name = set_element_names[name]
-			self.FUZZY_SET[i].append({"name"  : fuzzy_name,
-									 "xstart": xstart,
-									 "xend"  : xend,
-									 "midpoint": xend / xstart
-									})
-			k += next_point
+
+			xstart = k
+			if (((num_elements % 2) == ODD and
+				i == math.ceil(half_num_elements) - 1) or
+				i == 0
+			):
+				xend = xstart + next_half_point
+				self.FUZZY_SET.append({"name"  : fuzzy_name,
+									   "xstart": xstart,
+									   "xend"  : xend,
+									   "xmid"  : xstart + (next_half_point / 2) 
+									  })
+			else:
+				xend = xstart + next_point
+				self.FUZZY_SET.append({"name"  : fuzzy_name,
+									   "xstart": xstart,
+									   "xend"  : xend,
+									   "xmid"  : xstart + next_half_point
+									  })
+
+			k = xend
 			name += 2
 
 		# get second half of membership function points, every other starting
 		# at index 1
 		# for example: {FL, L, CL, IF, CR, R, FR} sets the elements for
 		#			   {L, IF, R}
-		k = set_xstart + 1
+		k = set_xstart
 		name = 1
-		for i in range(0, math.floor(num_elements / 2)):
-			xstart = k
-			xend   = k + next_point
+		for i in range(0, math.floor(half_num_elements)):
 			fuzzy_name = set_element_names[name]
-			self.FUZZY_SET[i].append({"name"  : fuzzy_name,
-									 "xstart": xstart,
-									 "xend"  : xend,
-									 "midpoint": xend / xstart
-									})
-			k += next_point
+
+			xstart = k
+			if ((num_elements % 2) == EVEN and  
+				i == math.floor(half_num_elements) - 1):
+				xend = xstart + next_half_point
+				self.FUZZY_SET.append({"name"  : fuzzy_name,
+									   "xstart": xstart,
+									   "xend"  : xend,
+									   "xmid"  : xstart + (next_half_point / 2)
+									  })
+			else:
+				xend = xstart + next_point
+				self.FUZZY_SET.append({"name"  : fuzzy_name,
+									   "xstart": xstart,
+									   "xend"  : xend,
+									   "xmid"  : xstart + next_half_point
+									  })
+			k = xend
 			name += 2
 
-		print(f"Pre-sort fuzzy set - {self.FUZZY_SET}")
-		
 		# sets FUZZY_SET in the same order as the set of element names passed
 		# into init. The lamba function returns i as the key.
 		#
@@ -86,9 +113,36 @@ class fuzzy_set:
 			key=lambda i: set_element_names.index(i["name"])
 		)
 
-		print(f"post-sort fuzzy set - {self.FUZZY_SET}")
-
 class fuzzy_controller:
 	def __init__(self, arduino_serial, kin):
 		self.arduino_serial = arduino_serial
 		self.kin = kin
+
+		# lists of the names for elements in the fuzzy set, used to set
+		# names of the fuzzy set and also used in fuzzy_set to sort its
+		# resulting list of dictionaries that stores the set element info
+		self.fuzzy_hand_names = ["at (AT)", "not at (NAT)"]
+		self.fuzzy_base_names = [
+								 "far left (FL)", 
+								 "left (L)", 
+								 "close left (CL)",
+								 "in front (IF)", 
+								 "close right (CR)",
+								 "right (R)",
+								 "far right (FR)"
+							   ]
+		self.fuzzy_arm_names = [
+							    "far behind hand (FBH)",
+								"close behind hand (CBH)",
+								"very close behind hand (VCBH)",
+								"at hand (AH)",
+								"very close front hand (VCFH)",
+								"close front hand (CFH)",
+								"far front hand (FFH)"
+							   ]
+
+		self.fuzzy_arm_set  = fuzzy_set("arm", 0, 10, self.fuzzy_arm_names)
+		self.fuzzy_base_set = fuzzy_set("base", 0, 12, self.fuzzy_base_names)
+		self.fuzzy_hand_set = fuzzy_set("hand", 0, 1, self.fuzzy_hand_names)
+		
+
